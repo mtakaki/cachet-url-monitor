1061b57169b10db97b027e21ed8ffc23fb2cc174
Mitsuo Takaki <mitsuotakaki@gmail.com>
2019-11-18 00:42:23 -0800

    Setting tox, maybe coverage will work...

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #from distutils.core import setup
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='cachet-url-monitor',
       version='0.5.1',
@@ -8,13 +8,19 @@ setup(name='cachet-url-monitor',
       author='Mitsuo Takaki',
       author_email='mitsuotakaki@gmail.com',
       url='https://github.com/mtakaki/cachet-url-monitor',
-      packages=['cachet_url_monitor'],
+      packages=find_packages(),
       license='MIT',
-      requires=[
-          'requests',
-          'yaml',
-          'schedule',
+      install_requires=[
+          'PyYAML==5.1.2',
+          'requests==2.22.0',
+          'schedule==0.6.0'
       ],
       setup_requires=["pytest-runner"],
-      tests_require=["pytest"]
+      tests_require=[
+          "pytest",
+          "codacy-coverage",
+          "mock",
+          "pytest-cov",
+          "coverage"
+          ]
       )
