b712eda001961d59e24dc0764b5cc376b193dcfe
mtakaki <mtakaki@users.noreply.github.com>
2020-01-19 14:25:29 -0800

    Coveralls (#79)
    * Trying to fix coveralls and upgrading the packages
    
    * Trying to fix coveralls and upgrading the packages

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #from distutils.core import setup
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='cachet-url-monitor',
       version='0.6.3',
@@ -8,13 +8,14 @@ setup(name='cachet-url-monitor',
       author='Mitsuo Takaki',
       author_email='mitsuotakaki@gmail.com',
       url='https://github.com/mtakaki/cachet-url-monitor',
-      packages=['cachet_url_monitor'],
+      packages=find_packages(),
       license='MIT',
       requires=[
           'requests',
           'yaml',
           'schedule',
+          'Click',
       ],
       setup_requires=["pytest-runner"],
-      tests_require=["pytest"]
+      tests_require=["pytest", "requests-mock"]
       )
