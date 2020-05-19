829155d3db3bc90589b7cb4702d809286de05894
mtakaki <mtakaki@users.noreply.github.com>
2020-05-19 01:38:56 -0700

    #99 add aws secrets manager plugin (#102)
    * #99 - Adding support to AWS Secrets Manager as a token provider
    
    * #99 - Removing unnecessary comments
    
    * #99 - Adding boto3 to setup.py
    
    * #99 - Adding secret_key parameter
    
    * #99 - Covering unhappy path when secret_key is incorrect

--- cachet_url_monitor/plugins/token_provider.py
+++ cachet_url_monitor/plugins/token_provider.py
@@ -1,8 +1,11 @@
+import json
+import os
 from typing import Any
 from typing import Dict
 from typing import Optional
 
-import os
+from boto3.session import Session
+from botocore.exceptions import ClientError
 
 
 class TokenProvider:
@@ -31,9 +34,49 @@ class ConfigurationFileTokenProvider(TokenProvider):
         return self.token
 
 
+class AwsSecretsManagerTokenRetrievalException(Exception):
+    def __init__(self, message):
+        self.message = message
+
+    def __repr__(self):
+        return self.message
+
+
+class AwsSecretsManagerTokenProvider(TokenProvider):
+    def __init__(self, config_data: Dict[str, Any]):
+        self.secret_name = config_data["secret_name"]
+        self.region = config_data["region"]
+        self.secret_key = config_data["secret_key"]
+
+    def get_token(self) -> Optional[str]:
+        session = Session()
+        client = session.client(service_name="secretsmanager", region_name=self.region)
+        try:
+            get_secret_value_response = client.get_secret_value(SecretId=self.secret_name)
+        except ClientError as e:
+            if e.response["Error"]["Code"] == "ResourceNotFoundException":
+                raise AwsSecretsManagerTokenRetrievalException(f"The requested secret {self.secret_name} was not found")
+            elif e.response["Error"]["Code"] == "InvalidRequestException":
+                raise AwsSecretsManagerTokenRetrievalException("The request was invalid")
+            elif e.response["Error"]["Code"] == "InvalidParameterException":
+                raise AwsSecretsManagerTokenRetrievalException("The request had invalid params")
+        else:
+            if "SecretString" in get_secret_value_response:
+                secret = json.loads(get_secret_value_response["SecretString"])
+                try:
+                    return secret[self.secret_key]
+                except KeyError:
+                    raise AwsSecretsManagerTokenRetrievalException(f"Invalid secret_key parameter: {self.secret_key}")
+            else:
+                raise AwsSecretsManagerTokenRetrievalException(
+                    "Invalid secret format. It should be a SecretString, instead of binary."
+                )
+
+
 TYPE_NAME_TO_CLASS: Dict[str, TokenProvider] = {
     "ENVIRONMENT_VARIABLE": EnvironmentVariableTokenProvider,
     "TOKEN": ConfigurationFileTokenProvider,
+    "AWS_SECRETS_MANAGER": AwsSecretsManagerTokenProvider,
 }
 
 
